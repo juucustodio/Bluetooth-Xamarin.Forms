7762ef884f83d8305b95059203c5bd37500acc73
Juliano Custódio <juliano.custodio@hotmail.com.br>
2017-12-06 18:45:10 -0200

    Implement list of device and search

--- DemoBluetooth/DemoBluetooth/MainPage.xaml.cs
+++ DemoBluetooth/DemoBluetooth/MainPage.xaml.cs
@@ -1,17 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Plugin.BLE;
+using Plugin.BLE.Abstractions.Contracts;
 using Xamarin.Forms;
 
 namespace DemoBluetooth
 {
     public partial class MainPage : ContentPage
     {
+
+        IAdapter adapter;
+        IBluetoothLE bluetoothBLE;
+        public  ObservableCollection<IDevice> list = new ObservableCollection<IDevice>();
+        private IDevice device;
         public MainPage()
         {
             InitializeComponent();
+
+            bluetoothBLE = CrossBluetoothLE.Current;
+            adapter = CrossBluetoothLE.Current.Adapter;
+
+            list = new ObservableCollection<IDevice>();
+
+            DevicesList.ItemsSource = list;
+            
         }
+
+        
+
+        private async void searchDevice(object sender, EventArgs e)
+        {
+            if (bluetoothBLE.State == BluetoothState.Off)
+            {
+                await DisplayAlert("Atenção", "Bluetooth desabilitado.", "OK");
+            }
+            else
+            {
+                list.Clear();
+                
+                adapter.ScanMode = ScanMode.Balanced;
+
+                adapter.DeviceDiscovered += (obj, a) =>
+                {
+                    if (!list.Contains(a.Device))
+                        list.Add(a.Device);
+                };
+
+                await adapter.StartScanningForDevicesAsync();
+                
+            }
+
+        }
+
+
     }
 }
